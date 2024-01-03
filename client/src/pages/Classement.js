03d83d32e86a0e5bde9a0edf9f79c0e63118166e
Jeremie-Lagache <jeremielagache3@mail.com>
2024-01-03 18:22:29 +0100

    update

--- client/src/pages/Classement.js
+++ client/src/pages/Classement.js
@@ -56,7 +56,7 @@ const Classement = () => {
                 {
                 usersData
                     .slice() 
-                    .sort((a, b) => a.tokens - b.tokens) 
+                    .sort((a, b) => b.tokens - a.tokens) 
                     .map((user, index) => (
                     <tr key={index}>
                         <td>{index + 1}</td>
