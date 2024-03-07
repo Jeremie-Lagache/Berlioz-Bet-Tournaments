85588aaf9abbe24b889198ab2ff50c37671e83c2
Jeremie-Lagache <jeremielagache3@mail.com>
2024-03-07 18:27:15 +0100

    update

--- server/Routes/paris.js
+++ server/Routes/paris.js
@@ -19,42 +19,22 @@ exports.createParis = async (req, res) => {
 
     const match = await Match.findOne({ _id: pari.match });
 
-    let updates = []
-
     let cotesCount = 1;
     match.counts.forEach((count) => {
-      let query;
-      cotesCount += isNaN(count) ? 0 : match.counts[count];
-      console.log(match.counts[count]);
+      cotesCount += isNaN(count) ? 0 : count;
     });
 
-    match.counts.forEach((count) => {
-      let query;
-      if (count === index) {
-        query = ((match.counts[count] + 1) / (cotesCount - match.counts[count])) * 100;
-      } else {
-        query = ((match.counts[count]) / (cotesCount - match.counts[count])) * 100;
-      }
-      updates.push(query)
-    });
-
+    let update = ((match.counts[index] + 1) / (cotesCount - match.counts[index] + 1)) * 100;
     console.log(cotesCount);
-    console.log(updates);
 
-    const updateQuerys = [];
-    updates.forEach((update) => {
-      let query = {}
-      query['cotes.' + update] = updates[update];
-      updateQuerys.push(query)
-    })
+    const updateQuery = {};
+    updateQuery['cotes.' + index] = update;
     const countquery = {};
     countquery['counts.' + index] = match.counts[index] + 1;
 
-    console.log(updateQuerys)
-
     await Match.updateOne(
       { _id: pari.match },
-      { $set: { ...updateQuerys[0], ...updateQuerys[1], ...updateQuerys[2], ...countquery } }
+      { $set: { ...updateQuery, ...countquery } }
     );
 
     res.json({ status: 'ok' });
